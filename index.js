636535dfbd9bf8a593470144be2bd31e0f4894c5
asyks <aaron@aaronsykes.com>
2020-10-05 21:01:38 -0700

    Fix worker terminate, and add logging

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+log = console.log;
+
 function workerFindPrimes() {
   const worker = new Worker("worker.js");
 
   worker.postMessage("discover");
+  log("main: worker started");
+
   worker.onmessage = (event) => {
-    if (event.data.value === undefined) {
-      worker.terminate();
-    } else {
+    if (event.data.done === false && typeof event.data.value === "number") {
       document.getElementById("result").textContent = event.data.value;
+    } else if (event.data.done === true) {
+      worker.terminate();
+      log("main: worker terminated");
+    } else {
+      log("main: ERROR invalid mesage recieved from worker");
     }
   };
 }
--- worker.js
+++ worker.js
@@ -1,15 +1,10 @@
-self.importScripts("primes.js");
+importScripts("primes.js");
 
 onmessage = function (event) {
   if (event.data === "discover") {
     const generator = primes();
-    while (true) {
-      prime = generator.next();
-      if (prime.value === undefined) {
-        break;
-      } else {
-        postMessage({ done: false, value: prime.value });
-      }
+    for (prime of generator) {
+      postMessage({ done: false, value: prime.value });
     }
     postMessage({ done: true, value: undefined });
   }
